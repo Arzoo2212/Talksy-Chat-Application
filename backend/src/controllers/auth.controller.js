59be8027af97ff86a2adbf1d48050c6e266a9605
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-07 01:48:06 +0530

    backend setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export const signup=(req,res)=>{
+    const 
+ try{
+
+ }catch(error){
+
+ }
+};
+export const login=(req,res)=>{
+
+};
+export const logout=(req,res)=>{
+
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,13 @@
 import express from "express"
+import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import { ConnectDB } from "./lib/db.js";
+dotenv.config();
 const app=express();
-const port=5001;
+app.use(express.json());
+let port=process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 app.listen(port,()=>{
     console.log(`Server ${port} started`);
+    ConnectDB(); 
 });
