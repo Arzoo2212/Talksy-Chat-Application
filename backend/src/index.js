09f6b8ee8376370f1caf4c88acaae82df217e576
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-07 00:16:33 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,8 @@
+import express from "express"
+import authRoutes from "./routes/auth.route.js"
+const app=express();
+const port=5001;
+app.use("/api/auth",authRoutes)
+app.listen(port,()=>{
+    console.log(`Server ${port} started`);
+});
