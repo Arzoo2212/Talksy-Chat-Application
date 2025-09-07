961ce3ba53d311ab913c0050f0991734eb6441dc
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-07 16:31:32 +0530

    signup setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,35 @@
-export const signup=(req,res)=>{
-    const 
- try{
-
- }catch(error){
-
+import { generateToken } from "../lib/utils.js";
+import User  from "../models/user.model.js"
+export const signup=async(req,res)=>{
+    const {fullName,email,password}=req.body;
+    try{
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be at least 6 characters"});
+        }
+        const user= await User.findOne({email});
+        if(user) return res.status(400).json({message:"Email already exists"});
+        const salt=await bcrypt.genSalt(10);
+        const hassedPassword=await bcrypt.hash(password,salt);
+        const newUser=new User({
+            fullName,
+            email,
+            password:hassedPassword 
+        })
+        if(newUser){
+              generateToken(newUser._id,res)
+              await newUser.save();
+              res.status(201).json({
+                _id:newUser,fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+              });
+        }else{
+            res.status(400).json({message:"Invalid user data"});
+        }
+    }
+ catch(error){
+           console.log("Error in signup controller",error.message);
+           res.status(500),json({message:"Internal Server Error"});
  }
 };
 export const login=(req,res)=>{
