945599b21c503beafa3c9f7342d0114acaf542c2
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-08 13:55:23 +0530

    backend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,11 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import { ConnectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 dotenv.config();
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 let port=process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 app.listen(port,()=>{
