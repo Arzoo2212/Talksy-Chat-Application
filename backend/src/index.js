90e218f5042296417d762a20619d1463201c171d
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-15 15:19:37 +0530

    commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import { ConnectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 const app=express();
@@ -11,6 +12,10 @@ app.use(cookieParser());
 let port=process.env.PORT || 5001;
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes);
+app.use(cors({
+    origin:" http://localhost:5173",
+    credentials:true,
+}))
 app.listen(port,()=>{
     console.log(`Server ${port} started`);
     ConnectDB(); 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,19 @@
 import {create} from "zustand"
- export const useAuthStore=create((set))=>(
-    {
-    authUser:null,
-    isCheckingAuth:true,
- }
- )
+import { axiosInstance } from "../lib/axios"
+ export const useAuthStore=create ((set)=>({
+   authUser:null,
+   isSigningUp:false,
+   isLoggingIn:false,
+   isUpdatingProfile:false,
+   isCheckingAuth:true,
+   checkAuth:async()=>{
+      try{
+        const res=await axiosInstance.get("/auth/check");
+        set({authUser:res.data});
+      }catch(error){
+           set({authUser:null});
+      }finally{
+         set({isCheckingAuth:false});
+      }
+   },
+ }));
