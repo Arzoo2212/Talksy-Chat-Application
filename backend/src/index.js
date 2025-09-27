be8438509919ae74ab60e3b5020a1bbb0ba0abe5
Arzoo <dadhwal.arzoo@gmail.com>
2025-09-27 17:05:55 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,12 +32,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+if(process.env.NODE_ENV === "production"){
+  const frontendDistPath = path.join(__dirname, "dist"); // backend/dist
+  app.use(express.static(frontendDistPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendDistPath, "index.html"));
   });
 }
+
 
 // Start server
 server.listen(PORT, () => {
